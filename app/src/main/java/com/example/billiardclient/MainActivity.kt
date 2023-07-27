3fb7b322f86d0927c0ec5c6730931392b15f6a4e
ldj526 <59276421+ldj526@users.noreply.github.com>
2023-07-27 17:12:19 +0900

    feat: 메인 화면을 전체화면으로 바꾸고 상단바 제거 기능 추가

--- app/src/main/java/com/example/billiardclient/MainActivity.kt
+++ app/src/main/java/com/example/billiardclient/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.billiardclient
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.billiardclient.databinding.ActivityMainBinding
@@ -37,6 +41,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         socket = Socket()
+
+        setFullScreen()
 
         tableNumber = getSharedPreferences("number", MODE_PRIVATE)
         ipAddress = getSharedPreferences("ip", MODE_PRIVATE)
@@ -336,6 +342,44 @@ class MainActivity : AppCompatActivity() {
 
         binding.totalHourTimeTv.text = totalTimeHourFormat
         binding.totalMinutesTimeTv.text = totalTimeMinutesFormat
+    }
+
+    // 앱을 전체화면으로 만들기
+    fun setFullScreen(){
+        // R 버전 이상
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            // 상단 ActionBar 제거
+            supportActionBar?.hide()
+
+            /* True -> 내부적으로 SYSTEM UI LAYOUT FLAG 값들을 살펴보고
+            해당 설정 값들을 토대로 화면을 구성하게 된다.
+            따라서 False로 설정을 해주어야 이제 사라질(Deprecated) Flag 값들을
+            무시하고 Window Insets를 통해 화면을 구상하게 된다.
+             */
+            window.setDecorFitsSystemWindows(false)
+            val controller = window.insetsController
+            if (controller != null) {
+                // 상태바와 네비게이션 사라지게 한다.
+                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                // Swipe 했을 경우에만 시스템 바 보이게 설정
+                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+
+        }
+        // R 버전 이하
+        else {
+            supportActionBar?.hide()
+            // 전체 화면 적용
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                    // 컨텐츠를 시스템 바 밑에 보이도록
+                    // 시스템바가 숨겨지거나 보여질 때 컨텐츠 부분이 리사이징 되는 것을 막기 위함
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    // 네비게이션과 상태바 사라지게
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
     }
 
     // 앱 종료 시
